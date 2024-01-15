1cc02f2478a4851a3d6b04851a51bedebdefac38
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2024-01-14 19:48:03 -0800

    created JSX files

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,11 @@
+export const MainView = () => {
+    return (
+      <div>
+        <div>myFlix Application</div>
+        <div>A CRUD API to </div>
+        <div>JavaScript: The Good Parts</div>
+        <div>JavaScript: The Definitive Guide</div>
+        <div>The Road to React</div>
+      </div>
+    );
+  };
