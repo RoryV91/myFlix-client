8e368dd2c7df850e5f4568af3cc4df21feef5e81
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2024-01-23 13:36:04 -0800

    install bootstrap, react bootstrap, start to refactor movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 const MovieCard = ({ movie, onMovieClick }) => {
-    return <div className="movie-card"
-                onClick={ () => 
-                    onMovieClick(movie)
-                }
-        >
-        {movie.title}
-    </div>;
+	return (
+		<Card
+			className="movie-card"
+			onClick={() => onMovieClick(movie)}
+		>
+			<Card.imageurl src={movie.imageurl} />
+			<Card.Body>
+				<Card.Title>{movie.title}</Card.Title>
+				<Card.Text>{movie.description}</Card.Text>
+			</Card.Body>
+		</Card>
+	);
 };
 
 MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        imageurl: PropTypes.string,
-        featured: PropTypes.bool,
-        actor_ids: PropTypes.arrayOf(PropTypes.string),
-        release: PropTypes.string,
-        director_ids: PropTypes.arrayOf(PropTypes.string),
-        genre_ids: PropTypes.arrayOf(PropTypes.string)
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired,
+	movie: PropTypes.shape({
+		title: PropTypes.string.isRequired,
+		description: PropTypes.string.isRequired,
+		imageurl: PropTypes.string,
+		featured: PropTypes.bool,
+		actor_ids: PropTypes.arrayOf(PropTypes.string),
+		release: PropTypes.string,
+		director_ids: PropTypes.arrayOf(PropTypes.string),
+		genre_ids: PropTypes.arrayOf(PropTypes.string),
+	}).isRequired,
+	onMovieClick: PropTypes.func.isRequired,
 };
 
 export default MovieCard;
