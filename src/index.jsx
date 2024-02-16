45431519d6276a3c4693364bb081fc482e589b9c
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2024-02-15 18:31:47 -0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import MainView from "./components/main-view/main-view";
 import "bootstrap/dist/css/bootstrap.min.css";
